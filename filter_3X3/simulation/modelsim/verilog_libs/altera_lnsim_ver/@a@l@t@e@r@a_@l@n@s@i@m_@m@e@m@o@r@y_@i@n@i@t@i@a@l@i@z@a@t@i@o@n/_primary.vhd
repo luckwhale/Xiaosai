e1b161f7bb00065dee133713e1e30ef64966dd67
luckwhale <2682581823a@gmail.com>
2023-02-23 22:03:17 +0800

    fourth commit

--- filter_3X3/simulation/modelsim/verilog_libs/altera_lnsim_ver/@a@l@t@e@r@a_@l@n@s@i@m_@m@e@m@o@r@y_@i@n@i@t@i@a@l@i@z@a@t@i@o@n/_primary.vhd
+++
@@ -1,4 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity ALTERA_LNSIM_MEMORY_INITIALIZATION is
-end ALTERA_LNSIM_MEMORY_INITIALIZATION;
--- filter_3X3/simulation/modelsim/verilog_libs/altera_lnsim_ver/fourteennm_iopll_functions/_primary.vhd
+++
@@ -1,4 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity fourteennm_iopll_functions is
-end fourteennm_iopll_functions;
